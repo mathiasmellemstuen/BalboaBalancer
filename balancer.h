e03844d1d9d4245cbeeeb32a604de49a6a7ffd6a
Tobias Hallingstad <tobias@hallingstad.me>
2021-10-23 00:01:19 +0200

    rework of header file

--- balancer.h
+++ balancer.h
@@ -1,8 +1,6 @@
 #ifndef BALANCER
 #define BALANCER
 
-#include <math.h>
-
 // Struct to hold left and right values. For more somple code
 struct LAR {
     int left = 0;
@@ -46,75 +44,12 @@ struct Vars {
     bool running = false;
 };
 
-int imu = 0;
-
 Vars vars;
 
-void Setup() {
-    int total = 0;
-
-    for (int i = 0; i < CALIBRATION_ITERATIONS; i++) {
-        // Read IMU
-        // total += imu.g.y
-    }
-
-    vars.GYZero = total / CALIBRATION_ITERATIONS;
-    vars.calibated = true;
-}
-
-void Start() {
-    if (vars.calibated && !vars.running) {
-        vars.running = true;
-        // Sett time for next update
-        // update loop() In thread?
-    } else {
-        // There is a problem
-    }
-}
-
-void Stop() {
-    if (vars.running) {
-        vars.running = false;
-        // Join thread
-    }
-}
-
-void Reset() {
-    vars.motorSpeed = 0;
-    ResetEncoders();
-
-    if (std::abs(vars.angle_rate) < 2) {
-        if (imu > 0) { // imu.a.z
-            vars.angle = 110;
-        } else {
-            vars.angle = -110;
-        }
-    }
-}
-
-void ResetEncoders() {
-    vars.distance = {0,0};
-}
-
-void StandUp() {
-    if (vars.calibated) {
-        int sign = 1;
-
-        if (imu == 0) { // imu.a.z
-            sign = -1;
-        }
-
-        Stop();
-        Reset();
-        // imu.read()
-        // Motors(sign*MOTOR_SPEED_LIMIT, -sign*MOTOR_SPEED_LIMIT)
-        // Wait for 0.4s
-        // Motors(-sign*MOTOR_SPEED_LIMIT, sign*MOTOR_SPEED_LIMIT)
-
-        for (int i = 0 ; i <= 40; i++) {
-            
-        }
-    }
-}
+void Setup();
+void IsBalancing();
+void Update();
+void UpdateSensors();
+void ResetEncoders();
 
 #endif
