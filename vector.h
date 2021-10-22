f417de4a6eaacc9bf77278716df492a0d13c0846
Mathias Mellemstuen <mathias.mellemstuen@gmail.com>
2021-10-23 00:35:47 +0200

    Added Vector2 and Vector3

---
+++ vector.h
@@ -0,0 +1,15 @@
+#ifndef BALBOA_BALANCER_VECTOR
+#define BALBOA_BALANCER_VECTOR
+
+struct Vector2 {
+    float x; 
+    float y;
+};
+
+struct Vector3 {
+    float x;
+    float y; 
+    float z;
+};
+
+#endif
