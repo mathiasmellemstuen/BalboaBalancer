2485beb160995f0cd186d69d89fea7ada87ad3a3
Tobias Hallingstad <tobias@hallingstad.me>
2021-10-22 23:49:40 +0200

    work on base functions

--- Documentation.md
+++ Documentation.md
@@ -11,4 +11,16 @@
 ```json 
     "arduino.path": "/snap/arduino/current"
 ```
-5. Type in the command ">Arduino: Change board type". Select Arduino Leonardo.
+5. Install the `Balboa32U4` library
+6. Type in the command ">Arduino: Change board type". Select Arduino Leonardo.
+
+## Mac
+1. Install VSCode
+2. Install Arduino vscode extension
+3. Install Arduino IDE
+4. In VSCode settings, set the arduino path:
+```json 
+    "arduino.path": "/Application"
+```
+5. Install the `Balboa32U4` library
+6. Type in the command ">Arduino: Change board type". Select Arduino Leonardo.
--- balancer.h
+++ balancer.h
@@ -1,26 +1,113 @@
 #ifndef BALANCER
 #define BALANCER
 
+#include <math.h>
+
 // Struct to hold left and right values. For more somple code
 struct LAR {
     int left = 0;
     int right = 0;
 };
 
-struct Consts {
-    int GEAR_RATIO = 111;
-    int MOTOR_SPEED_LIMIT = 300;
-    int ANGLE_RATE_RATIO = 140;
-    int ANGLE_RESPONSE = 12;
-    int DISTANCE_RESPONSE = 90;
-    int DISTANCE_DIFF_RESPONSE = -50;
-    int SPEED_RESPONSE = 3300;
-    int CALIBRATION_ITERATIONS = 100;
+enum Consts {
+    GEAR_RATIO = 111,
+    MOTOR_SPEED_LIMIT = 300,
+    ANGLE_RATE_RATIO = 140,
+    ANGLE_RESPONSE = 12,
+    DISTANCE_RESPONSE = 90,
+    DISTANCE_DIFF_RESPONSE = -50,
+    SPEED_RESPONSE = 3300,
+    CALIBRATION_ITERATIONS = 100,
 
-    float UPDATE_TIME = 0.01;
+    UPDATE_TIME = 1 // Deviden by 100 in H<
 };
 
+struct Vars {
+    float GYZero = 0.0;
+    int angle = 0;
+    int angle_rate = 0;
+    int nextUpdate = 0;
+    int motorSpeed = 0;
 
+    LAR speed;
+    LAR drive;
+    LAR lastCounts;
+    LAR distance;
 
+    bool calibated = false;
+    bool balancing = false;
+    bool running = false;
+};
+
+int imu = 0;
+
+Vars vars;
+
+void Setup() {
+    int total = 0;
+
+    for (int i = 0; i < CALIBRATION_ITERATIONS; i++) {
+        // Read IMU
+        // total += imu.g.y
+    }
+
+    vars.GYZero = total / CALIBRATION_ITERATIONS;
+    vars.calibated = true;
+}
+
+void Start() {
+    if (vars.calibated && !vars.running) {
+        vars.running = true;
+        // Sett time for next update
+        // update loop() In thread?
+    } else {
+        // There is a problem
+    }
+}
+
+void Stop() {
+    if (vars.running) {
+        vars.running = false;
+        // Join thread
+    }
+}
+
+void Reset() {
+    vars.motorSpeed = 0;
+    ResetEncoders();
+
+    if (std::abs(vars.angle_rate) < 2) {
+        if (imu > 0) { // imu.a.z
+            vars.angle = 110;
+        } else {
+            vars.angle = -110;
+        }
+    }
+}
+
+void ResetEncoders() {
+    vars.distance = {0,0};
+}
+
+void StandUp() {
+    if (vars.calibated) {
+        int sign = 1;
+
+        if (imu == 0) { // imu.a.z
+            sign = -1;
+        }
+
+        Stop();
+        Reset();
+        // imu.read()
+        // Motors(sign*MOTOR_SPEED_LIMIT, -sign*MOTOR_SPEED_LIMIT)
+        // Wait for 0.4s
+        // Motors(-sign*MOTOR_SPEED_LIMIT, sign*MOTOR_SPEED_LIMIT)
+
+        for (int i = 0 ; i <= 40; i++) {
+            
+        }
+    }
+}
 
 #endif
